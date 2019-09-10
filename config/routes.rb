45d468372c78ef1d3130877f835b4dfba79d2475
NaoIkeji <na3o.g.7@gmail.com>
2019-09-10 11:12:58 +0000

    [Finish]4-8_投稿/編集/削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+	root'books#top'
+
 	resources :books
 
   #get 'books/index'
   #get 'books/show'
   #get 'books/new'
   #get 'books/edit'
+
+  #get 'books/:id' => 'books#index', as: 'books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
